cc4d3aeca3fd7f3586d2753393c315cc05f90659
Chett Tiller <chett.tiller@gmail.com>
2018-09-24 15:35:49 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,6 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  if name_hash.length = 1
-    return conditional
-  end
   i = 10000
   name = ""
   name_hash.each do |k,v|
